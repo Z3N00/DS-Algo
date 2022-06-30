d0220897521b62d40c28dad2c9dbb19ca524993c
Z3N00 <kartikgupta676@gmail.com>
2022-06-30 01:21:22 -0400

    sliding window question

--- 11. Stacks and Queues/slidingWindowMax.py
+++ 11. Stacks and Queues/slidingWindowMax.py
@@ -2,20 +2,63 @@ def slidingWindow(arr, k1):
     # write your code here
     st = []
     index = 0
-    while index <= len(arr):
-        if(len(arr) - index <= k1):
-            st.append(max(arr[index:len(arr)+1]))
+    while index < len(arr):
+        if(len(arr) - index >= k1):
+            st.append(max(arr[index:k1+index]))
+            index = index + 1
         else:
-            st.append(max(arr[i:k1+i+1]))
+            break
+           
 
-    print(st)
+    for i in st:
+        print(i)
 
 
-n = int(input())
-arr = [] * n
-for i in range(n) :
-    x = int(input())
-    arr.append(x)
-k = int(input())
+# n = int(input())
+# arr = [] * n
+# for i in range(n) :
+#     x = int(input())
+#     arr.append(x)
+# k = int(input())
 
-slidingWindow(arr, k)
+slidingWindow([1,2,3,4], 2)
+
+""" pep coding code by sliding window"""
+
+
+# def slidingWindow(arr, k1):
+#   st = []
+#        n = len(arr)
+#            ngr = [0] * n     # next greater elemment on right
+#                  for i in reversed(range(n - 1)) :
+#                while len(st) > 0 and arr[i] >= arr[st[-1]]:
+#                      st.pop()
+#                if len(st) == 0:
+#                        ngr[i] = (n)
+#                         else:
+#                                   ngr[i] = (st[-1])
+#                                            st.append(i)
+
+
+
+#                                            ans = [0] * (n - k1 + 1)
+#                                                  j = 0
+#                                                      for i in range(n - k1 + 1) :
+#                                            if j < i:
+#                                                        j = i
+#                                                            ep = i + k1     # window size i + k
+#                                                    while ngr[j] < ep:
+#                                                                    j = ngr[j]
+#                                                                        ans[i] = arr[j]
+
+#                                                            for i in ans :
+#                                                                            print(i)
+
+#                                                                              n = int(input())
+#                                                                                  arr = [] * n
+#                                                                                      for i in range(n) :
+#                                                                                        x = int(input())
+#                                                                                            arr.append(x)
+#                                                                                            k = int(input())
+
+#                                                                                                slidingWindow(arr, k)
