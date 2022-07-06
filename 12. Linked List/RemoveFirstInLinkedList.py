9312369f341f0bcd857bf65aa20e0e92967df371
Z3N00 <kartikgupta676@gmail.com>
2022-07-06 17:49:31 -0400

    linked list

---
+++ 12. Linked List/RemoveFirstInLinkedList.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+        self.tail = None
+
+    def AddLast(self, newdata):
+        NewNode = Node(newdata)
+        if self.head is None:
+            self.head = NewNode
+            self.tail = NewNode
+            self.size += 1
+            return
+        laste = self.head
+        while (laste.next):
+            laste = laste.next
+            laste.next = NewNode
+            self.tail = laste.next
+            self.size += 1
+
+    def display(self):
+        printval = self.head
+        while printval is not None:
+            print(
+                printval.data, end=" ")
+            printval = printval.next
+            print(" ")
+
+    def testList(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data)
+            temp = temp.next
+            print(self.size)
+
+            if self.size > 0:
+                print(self.tail.data)
+
+    def removeFirst(self):
+        # write your code here
+        if self.size == 0:
+            print("List is empty")
+            return
+        temp = self.head
+        self.head = temp.next
+        self.size -= 1
+
+
+
+
+l1 = SLinkedList()
+while 1 > 0:
+    str = input()
+    if str[0] == 'q':
+        break
+
+    elif str[0] == 'a':
+        val = int(str[-3] + str[-2])
+        l1.AddLast(val)
+    elif str[0] == 'd':
+        l1.display()
+    elif str[0] == 's':
+        print(l1.size)
+    elif str[0] == 'r':
+        l1.removeFirst()
+
+
