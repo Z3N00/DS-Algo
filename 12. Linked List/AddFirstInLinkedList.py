ac34127f2d3df6115b0ff4fc948a3cbb8cb8ba09
Z3N00 <kartikgupta676@gmail.com>
2022-07-07 17:53:34 -0400

    remove last

---
+++ 12. Linked List/AddFirstInLinkedList.py
@@ -0,0 +1,112 @@
+class Node:
+    def __init__(self):
+        self.data = 0
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def addLast(self, val):
+        temp = Node()
+        temp.data = val
+        temp.next = None
+
+        if self.size == 0:
+            self.head = self.tail = temp
+        else:
+            self.tail.next = temp
+            self.tail = temp
+            self.size += 1
+
+    def size(self):
+        return self.size
+
+    def display(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+    def removeFirst(self):
+        if self.size == 0:
+            print("List is empty")
+        elif self.size == 1:
+            self.head = self.tail = None
+            self.size = 0
+        else:
+            self.head = self.head.next
+            self.size -= 1
+
+    def getFirst(self):
+        # write your code here
+        if self.size == 0:
+            print("List is empty")
+            return -1
+        else:
+            return self.head.data
+
+    def getLast(self):
+        # write your code here
+        if self.size == 0:
+            print("List is empty")
+            return -1
+        else:
+            return self.tail.data
+        
+    def getAt(self, idx):
+        # write your code here
+        if self.size == 0:
+            print("List is empty")
+            return -1
+        elif idx < 0 or idx > self.size:
+            print("Invalid arguments")
+            return -1
+
+    def addFirst(self, val):
+        # write your code here
+        newNode = Node()
+        newNode.data = val
+        newNode.next = self.head
+        self.head = newNode
+        self.size += 1
+        if self.size == 0:
+            self.tail = newNode
+            
+
+l1 = LinkedList()
+while True:
+    cmd = input().split(" ")
+    if "addLast" in cmd[0]:
+        val = int(cmd[1])
+        l1.addLast(val)
+
+    elif "getFirst" in cmd[0]:
+        ans = l1.getFirst()
+        if (ans != -1):
+            print(ans)
+
+    elif "getLast" in cmd[0]:
+        ans = l1.getLast()
+        if (ans != -1):
+            print(ans)
+
+    elif "getAt" in cmd[0]:
+        idx = int(cmd[1])
+        ans = l1.getAt(idx)
+        if (ans != -1):
+            print(ans)
+
+    elif "removeFirst" in cmd[0]:
+        l1.removeFirst()
+    
+    elif "addFirst" in cmd[0]:
+        val = int(cmd[1])
+        l1.addFirst(val)
+
+    elif "quit" in cmd[0]:
+        break
---
+++ 12. Linked List/test.py
@@ -0,0 +1,96 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def _size(self):
+        print(f"size of Linked list: {self.size}")
+
+    def addLast(self, val):
+        node = Node(val)
+
+        if self.size == 0:
+            self.head = self.tail = node
+            self.size += 1
+
+        else:
+            temp = self.tail
+            temp.next = node
+            self.tail = node
+            self.size += 1
+
+    def addFirst(self, val):
+        node = Node(val)
+        if self.size == 0:
+            self.head = self.tail = node
+            self.size += 1
+        else:
+            temp = self.head
+            self.head = node
+            node.next = temp
+            self.size += 1
+
+    def addAt(self,idx, val):
+        node = Node(val)
+        if idx < 0 or idx > self.size:
+            print("Invalid Argument")
+        elif idx == 0:
+            self.addFirst(val)
+        elif idx == self.size:
+            self.addLast(val)
+        else:
+            temp = self.head
+            i = 0
+            while i < idx - 1:
+                temp = temp.next
+                i += 1
+
+            node.next = temp.next
+            temp.next = node
+            self.size += 1
+
+    def giveAt(self, idx):
+        if self.size == 0:
+            print("List is empty")
+            
+        elif idx < 0 or idx > self.size:
+            print("Invalid arguments")
+            
+        else:
+            temp = self.head
+            i = 0
+            while i < idx:
+                temp = temp.next
+                i += 1
+            print(temp.data)
+
+    def removeFirst(self):
+        self.head = self.head.next
+        self.size -= 1
+
+    def display(self):
+        printval = self.head
+        while printval:
+            print(printval.data, "->", end=" ")
+            printval = printval.next
+        print()
+
+
+li = LinkedList()
+i = 10
+while i <= 20:
+    li.addLast(i)
+    i += 1
+
+li.addFirst(100)
+li.addAt(3, 200)
+li.giveAt(12)
+li.removeFirst()
+li.display()
+li._size()
